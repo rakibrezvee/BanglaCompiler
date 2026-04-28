3ded84f53f0133773c66d40f62cb6b0cf92cfce8
rakibrezvee <118680942+rakibrezvee@users.noreply.github.com>
2026-04-28 21:24:08 +0600

    Update SymbolTable.java

--- SymbolTable.java
+++ SymbolTable.java
@@ -3,23 +3,20 @@ import java.util.Map;
 
 public class SymbolTable {
     
-    // A simple class to hold a variable's data type
     public static class VariableInfo {
-        public String type; // e.g., "shongkha" or "kotha"
+        public String type; 
 
         public VariableInfo(String type) {
             this.type = type;
         }
     }
 
-    // The actual memory bank (HashMap)
     private Map<String, VariableInfo> table;
 
     public SymbolTable() {
         this.table = new HashMap<>();
     }
 
-    // Add a new variable to memory
     public void declareVariable(String name, String type) {
         if (table.containsKey(name)) {
             throw new RuntimeException("Syntax Error: Variable '" + name + "' is already declared!");
@@ -28,7 +25,6 @@ public class SymbolTable {
         System.out.println("Symbol Table: Saved variable '" + name + "' as type '" + type + "'");
     }
 
-    // Check what type a variable is (used for Type Checking)
     public String getVariableType(String name) {
         if (!table.containsKey(name)) {
             throw new RuntimeException("Syntax Error: Variable '" + name + "' has not been declared!");
