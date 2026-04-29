c6a9ecd6d4b3500681870ab054c720cde042e1b1
rakibrezvee <118680942+rakibrezvee@users.noreply.github.com>
2026-04-29 16:59:46 +0600

    Update SymbolTable.java

--- SymbolTable.java
+++ SymbolTable.java
@@ -3,7 +3,6 @@ import java.util.Map;
 
 public class SymbolTable {
     
-    
     public static class VariableInfo {
         public String type; 
 
@@ -12,26 +11,23 @@ public class SymbolTable {
         }
     }
 
-    
     private Map<String, VariableInfo> table;
 
     public SymbolTable() {
         this.table = new HashMap<>();
     }
 
-    
     public void declareVariable(String name, String type) {
         if (table.containsKey(name)) {
-            throw new RuntimeException("Syntax Error: Variable '" + name + "' is already declared!");
+            throw new RuntimeException("সিনট্যাক্স এরর: '" + name + "' ভেরিয়েবলটি আগেই ডিক্লেয়ার করা হয়েছে!");
         }
         table.put(name, new VariableInfo(type));
-        System.out.println("মেমোরি: '" + name + "' ভেরিয়েবলটি '" + type + "' হিসেবে সেভ করা হয়েছে।");
+        System.out.println("মেমোরি: '" + name + "' ভেরিয়েবলটি '" + type + "' হিসেবে সেভ করা হয়েছে।");
     }
 
-    
     public String getVariableType(String name) {
         if (!table.containsKey(name)) {
-            throw new RuntimeException("Syntax Error: Variable '" + name + "' has not been declared!");
+            throw new RuntimeException("সিনট্যাক্স এরর: '" + name + "' ভেরিয়েবলটি ডিক্লেয়ার করা হয়নি!");
         }
         return table.get(name).type;
     }
