e47b507823e2026482201c73fe412ea096e7c411
rakibrezvee <118680942+rakibrezvee@users.noreply.github.com>
2026-05-01 01:17:38 +0600

    Update SymbolTable.java

--- SymbolTable.java
+++ SymbolTable.java
@@ -2,32 +2,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SymbolTable {
-    
     public static class VariableInfo {
         public String type; 
-
         public VariableInfo(String type) {
             this.type = type;
         }
     }
 
-    private Map<String, VariableInfo> table;
-
-    public SymbolTable() {
-        this.table = new HashMap<>();
-    }
+    private Map<String, VariableInfo> table = new HashMap<>();
 
     public void declareVariable(String name, String type) {
         if (table.containsKey(name)) {
-            throw new RuntimeException("সিনট্যাক্স এরর: '" + name + "' ভেরিয়েবলটি আগেই ডিক্লেয়ার করা হয়েছে!");
+            throw new RuntimeException("সেম্যান্টিক এরর: '" + name + "' ভেরিয়েবলটি আগেই ডিক্লেয়ার করা হয়েছে!");
         }
         table.put(name, new VariableInfo(type));
-        System.out.println("মেমোরি: '" + name + "' ভেরিয়েবলটি '" + type + "' হিসেবে সেভ করা হয়েছে।");
     }
 
     public String getVariableType(String name) {
         if (!table.containsKey(name)) {
-            throw new RuntimeException("সিনট্যাক্স এরর: '" + name + "' ভেরিয়েবলটি ডিক্লেয়ার করা হয়নি!");
+            throw new RuntimeException("সেম্যান্টিক এরর: '" + name + "' ভেরিয়েবলটি ডিক্লেয়ার করা হয়নি!");
         }
         return table.get(name).type;
     }
